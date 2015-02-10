40c91900a9b0455bee8af8a033ffdc2e6720f2fd
David Nault <dnault@mac.com>
2015-02-09 16:39:59 -0800

    fix login for allowing missing patch file

--- src/main/groovy/com/github/dnault/xmlpatch/gradle/XmlPatchPlugin.groovy
+++ src/main/groovy/com/github/dnault/xmlpatch/gradle/XmlPatchPlugin.groovy
@@ -18,6 +18,7 @@ class XmlPatchPlugin implements Plugin<Project> {
                     throw new FileNotFoundException(message)
                 }
                 project.logger.info message
+                return;
             }
 
             project.configure(copySpec) {
